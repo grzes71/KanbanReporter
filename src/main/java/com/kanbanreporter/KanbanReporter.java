972465e5d8980ecc1f2241ef7fb5a56bc36786e1
grzeg <grzeg@LAPTOP-GRZES>
2017-02-16 21:04:59 +0100

    moved System.exit to main

--- src/main/java/com/kanbanreporter/KanbanReporter.java
+++ src/main/java/com/kanbanreporter/KanbanReporter.java
@@ -18,33 +18,34 @@ import com.kanbanreporter.reporter.TemplateReporter;
  */
 public class KanbanReporter {
 
-    private static final Logger LOGGER = Logger.getLogger(KanbanReporter.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(KanbanReporter.class.getName());
 
-    private KanbanReporter() {
+	private KanbanReporter() {
 		super();
 	}
 
 	public static void main(String[] args) {
-        LogManager.getLogManager().getLogger(Logger.GLOBAL_LOGGER_NAME).setLevel(Level.ALL);
-        LOGGER.info("Start KanbanReporter");
+		LogManager.getLogManager().getLogger(Logger.GLOBAL_LOGGER_NAME).setLevel(Level.ALL);
+		LOGGER.info("Start KanbanReporter");
 
-        if (args.length == 2) {
-            generateKanban(args[0], args[1]);
-        } else {
-            LOGGER.severe("Provide path to ini config and output file");
-        }
-    }
+		if (args.length == 2) {
+			try {
+				generateKanban(args[0], args[1]);
+			} catch (ImporterException | ReportException e) {
+				LOGGER.log(Level.SEVERE, "An error has occured", e);
+				System.exit(1);
+			}
+		} else {
+			LOGGER.severe("Provide path to ini config and output file");
+		}
+	}
 
-    private static void generateKanban(String csv_path, String output_path) {
-        Importer csv_importer = new CsvImporter(csv_path);
-        Reporter reporter = new TemplateReporter("template.html", output_path);
-        
-        try {
-            Kanban kanban = csv_importer.createKanban();
-            reporter.generate(kanban);
-        } catch (ImporterException | ReportException e) {
-            LOGGER.log(Level.SEVERE, "An error has occured", e);
-            System.exit(1);
-        }
-    }
+	private static void generateKanban(String csv_path, String output_path) throws ImporterException, ReportException {
+		Importer csv_importer = new CsvImporter(csv_path);
+		Reporter reporter = new TemplateReporter("template.html", output_path);
+
+		Kanban kanban = csv_importer.createKanban();
+		reporter.generate(kanban);
+
+	}
 }
